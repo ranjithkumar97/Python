b2e9cb8284993dee7a8772b94c2fbec6d4235bc9
Ranjith Kumar <37208730+ranjithkumar97@users.noreply.github.com>
2019-01-06 17:53:26 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="sandy",
+    database="sandy"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM user432")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
