8e655cffba906a11ea10bc8626330434fa68d190
santhosh kumar <sandysanthosh101@gmail.com>
2019-01-07 17:05:54 +0530

    python getting start

---
+++ operators.py
@@ -0,0 +1,40 @@
+print("Hello world") #This is a comment
+if(5>2):
+ print("Condition is true")
+ """This is a 
+ multiline docstring."""
+ print("hello") #get started variables
+a1="ranjith" #declare a variable with alphanumerical and underscore but not number.
+x = "Python is "
+print("  " + x+"osm")
+
+x = 1 #int
+y = 2.8 #float
+z = 1j #complex
+print(x,y,z)
+z = 1e2 #comment an "e" to indicate the power of 10
+print(z)
+""" This
+ is 
+ a casting"""
+int(5.6)
+print('ranjith')  #using single quotation
+b = "     Hello World!       "
+print(b[2:6]) #print the character position of 2 to 6
+print(b.strip()) #rremoves whitespace front or end
+
+print(len(b)) #print length of b
+print(b.lower()) #print lower similarly upper
+print(b.replace("Hello","ranjith")) #replace a word using variable and replace keyword
+print(b.split())
+
+print("Enter your name:")
+x = input()
+print("Hello, " + x) #user to enter name  from command
+a=100
+b=25
+z=a//b
+print(z) #by using floor divison eg 2 .0 is round it off to 2
+r=2**5
+print(r) #exponentiation for 2power 5
+
